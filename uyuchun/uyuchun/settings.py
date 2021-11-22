f6c30e3079b1b324e128c489122f9d0f18fa0287
alexthemaggot23@gmail.com <PN7V4czz25JVneD>
2021-11-22 22:44:22 +0500

    add mainapp to settings

--- uyuchun/uyuchun/settings.py
+++ uyuchun/uyuchun/settings.py
@@ -19,6 +19,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mainapp.apps.MainappConfig',
 ]
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
