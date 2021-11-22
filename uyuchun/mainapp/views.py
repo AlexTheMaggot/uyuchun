af41d22ddbb8aa380be4f40b06afb052a0f1e91a
alexthemaggot23@gmail.com <PN7V4czz25JVneD>
2021-11-22 23:05:23 +0500

    created index view

--- uyuchun/mainapp/views.py
+++ uyuchun/mainapp/views.py
@@ -1,3 +1,10 @@
 # Imports
 from django.shortcuts import render
 # End Imports
+
+
+# Config
+def index(request):
+    template = 'mainapp/index.html'
+    return render(request, template)
+# End Config
