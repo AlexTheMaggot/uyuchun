644a8acc63f2335516136deca81033c0d10b436c
alexthemaggot23@gmail.com <PN7V4czz25JVneD>
2022-02-09 10:47:24 +0500

    add: Product form Product Image form Category form SubCategory form

---
+++ uyuchun/mainapp/forms.py
@@ -0,0 +1,26 @@
+from django import forms
+from .models import Category, SubCategory, Product, ProductImage
+
+
+class CategoryForm(forms.ModelForm):
+    class Meta:
+        model = Category
+        fields = ('name', 'slug')
+
+
+class SubCategoryForm(forms.ModelForm):
+    class Meta:
+        model = SubCategory
+        fields = ('name', 'slug', 'category',)
+
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = ('name', 'slug', 'subcategory', 'description', 'price')
+
+
+class ProductImageForm(forms.ModelForm):
+    class Meta:
+        model = ProductImage
+        fields = ('img', 'product')
