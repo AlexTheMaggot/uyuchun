0eee76649b4bdace99b8921e795c6065f6e3a4ce
alexthemaggot23@gmail.com <PN7V4czz25JVneD>
2022-02-24 22:07:08 +0500

    add product list and category detail views

--- uyuchun/mainapp/views.py
+++ uyuchun/mainapp/views.py
@@ -1,7 +1,7 @@
 # Imports
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from admin.models import Header, IndexPage, Footer
-from .models import Category
+from .models import Category, Product
 # End Imports
 
 
@@ -24,5 +24,19 @@ def index(request):
     context = get_context()
     context['indexpage'] = IndexPage.objects.first()
     template = 'mainapp/index.html'
+    return render(request, template, context)
+
+
+def product_list(request):
+    context = get_context()
+    context['products'] = Product.objects.all()
+    template = 'mainapp/product_list.html'
+    return render(request, template, context)
+
+
+def category_detail(request, category_slug):
+    context = get_context()
+    context['category_detail'] = get_object_or_404(Category, slug=category_slug)
+    template = 'mainapp/category_detail.html'
     return render(request, template, context)
 # End Config
