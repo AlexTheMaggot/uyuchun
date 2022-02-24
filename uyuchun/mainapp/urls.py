c56eec6d5877ad0fbabcd69faed9735849b3d892
alexthemaggot23@gmail.com <PN7V4czz25JVneD>
2022-02-24 22:06:52 +0500

    add product list and category detail urls

--- uyuchun/mainapp/urls.py
+++ uyuchun/mainapp/urls.py
@@ -9,5 +9,8 @@ app_name = 'mainapp'
 urlpatterns = [
     path('', views.plug, name='plug'),
     path('testpage123/', views.index, name='index'),
+    path('shop/', views.product_list, name='product_list'),
+    path('shop/<slug:category_slug>/', views.category_detail, name='category_detail'),
+
 ]
 # End Config
