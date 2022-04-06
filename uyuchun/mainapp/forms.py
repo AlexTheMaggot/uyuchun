e84ddf46034a57d7c58abdb2493c89dc5effcc60
alexthemaggot23@gmail.com <PN7V4czz25JVneD>
2022-04-06 21:07:07 +0500

    add measure form

--- uyuchun/mainapp/forms.py
+++ uyuchun/mainapp/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Category, SubCategory, Product, ProductImage
+from .models import Category, SubCategory, Product, ProductImage, Measure
 
 
 class CategoryForm(forms.ModelForm):
@@ -24,3 +24,9 @@ class ProductImageForm(forms.ModelForm):
     class Meta:
         model = ProductImage
         fields = ('img', 'product')
+
+
+class MeasureForm(forms.ModelForm):
+    class Meta:
+        model = Measure
+        fields = ('name', 'short_name')
